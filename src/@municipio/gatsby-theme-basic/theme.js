2ef7843f2947c0c9b8196054aaf4dd9a71f64ce2
Fredrik Johansson <jeanfredrik@gmail.com>
2021-05-14 16:04:53 +0200

    chore(deps): update theme

--- src/@municipio/gatsby-theme-basic/theme.js
+++ src/@municipio/gatsby-theme-basic/theme.js
@@ -1,1 +1,8 @@
-export { default } from "../../theme";
+import parentTheme from "@municipio/gatsby-theme-basic/src/theme";
+import { mergeThemes } from "@whitespace/components";
+
+import childTheme from "../../theme";
+
+var theme = mergeThemes(parentTheme, childTheme);
+
+export default theme;
